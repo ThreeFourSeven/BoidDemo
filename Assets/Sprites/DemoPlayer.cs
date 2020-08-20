89b7d20ec6e912ec446a15d491973568c5c1a0b8
ThreeFourSeven <sevenHRandall@gmail.com>
2020-08-20 19:02:00 -0300

    Repo cleanup

--- Assets/Sprites/DemoPlayer.cs
+++ Assets/Sprites/DemoPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DemoPlayer : MonoBehaviour
 {
-    public GameObject boidObject;
+    public GameObject boidPrefab;
     public GameObject targetObject;
     public static Vector2 flockCenter = new Vector2();
     public static int count = 0;
@@ -15,7 +15,7 @@ public class DemoPlayer : MonoBehaviour
     {
         for (int i = 0; i < 500; i++)
         {
-            GameObject go = Instantiate(boidObject, new Vector3(), Quaternion.identity);
+            GameObject go = Instantiate(boidPrefab, new Vector3(), Quaternion.identity);
             go.GetComponent<OBoid>().GetComponent<OBoid>().targetObject = targetObject;
             go.GetComponent<OBoid>().GetComponent<OBoid>().position = new Vector2(Random.Range(-10,10), Random.Range(-10, 10));
             boids.Add(go);
@@ -30,15 +30,15 @@ public class DemoPlayer : MonoBehaviour
             flockCenter += go.GetComponent<OBoid>().position;
         count = boids.Count;
         flockCenter /= (count - 1);
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
-            GameObject go = Instantiate(boidObject, p, Quaternion.identity);
+            GameObject go = Instantiate(boidPrefab, p, Quaternion.identity);
             go.GetComponent<OBoid>().targetObject = targetObject;
             go.GetComponent<OBoid>().position = p;
             boids.Add(go);
         }
-        if (Input.GetMouseButton(1))
+        if (Input.GetMouseButton(0))
         {
             Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
             targetObject.transform.position = p;
